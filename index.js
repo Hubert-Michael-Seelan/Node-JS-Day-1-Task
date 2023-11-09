d97248d1176e455aadb2131828032d9f755feb5d
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-11-09 16:04:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs/promises");
+const express = require("express");
+const HTTP_SERVER = express();
+const PORT = 4000;
+
+const content = new Date().toString().replace(/[:.]/g, "-");
+const path = `./Files/${content}.txt`
+
+async function createFile() {
+  try {
+    await fs.writeFile(path , content);
+
+    console.log("hey all");
+  } catch (error) {
+    console.log(err);
+  }
+}
+createFile()
+
+HTTP_SERVER.listen(PORT, "localhost", () => {
+  console.log("Listening to port", PORT);
+});
+
+HTTP_SERVER.use("/", (req, res, next) => {
+  console.log("Request Hitted");
+  return res.status(200).json({
+    message: "Request Hitted Successfully"
+  })
+});
